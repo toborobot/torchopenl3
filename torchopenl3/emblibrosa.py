3c9d0cf8586e69194954366fbd4b2baa1773c785
toborobot_ru <develop@toborobot.ru>
2021-08-23 18:30:00 +0300

    added tiny

--- torchopenl3/emblibrosa.py
+++ torchopenl3/emblibrosa.py
@@ -1603,6 +1603,58 @@ def resample(
 
     return np.asfortranarray(y_hat, dtype=y.dtype)
 
+def tiny(x):
+    """Compute the tiny-value corresponding to an input's data type.
+    This is the smallest "usable" number representable in ``x.dtype``
+    (e.g., float32).
+    This is primarily useful for determining a threshold for
+    numerical underflow in division or multiplication operations.
+    Parameters
+    ----------
+    x : number or np.ndarray
+        The array to compute the tiny-value for.
+        All that matters here is ``x.dtype``
+    Returns
+    -------
+    tiny_value : float
+        The smallest positive usable number for the type of ``x``.
+        If ``x`` is integer-typed, then the tiny value for ``np.float32``
+        is returned instead.
+    See Also
+    --------
+    numpy.finfo
+    Examples
+    --------
+    For a standard double-precision floating point number:
+    >>> librosa.util.tiny(1.0)
+    2.2250738585072014e-308
+    Or explicitly as double-precision
+    >>> librosa.util.tiny(np.asarray(1e-5, dtype=np.float64))
+    2.2250738585072014e-308
+    Or complex numbers
+    >>> librosa.util.tiny(1j)
+    2.2250738585072014e-308
+    Single-precision floating point:
+    >>> librosa.util.tiny(np.asarray(1e-5, dtype=np.float32))
+    1.1754944e-38
+    Integer
+    >>> librosa.util.tiny(5)
+    1.1754944e-38
+    """
+
+    # Make sure we have an array view
+    x = np.asarray(x)
+
+    # Only floating types generate a tiny
+    if np.issubdtype(x.dtype, np.floating) or np.issubdtype(
+        x.dtype, np.complexfloating
+    ):
+        dtype = x.dtype
+    else:
+        dtype = np.float32
+
+    return np.finfo(dtype).
+
 def normalize(S, norm=np.inf, axis=0, threshold=None, fill=None):
     """Normalize an array along a chosen axis.
     Given a norm (described below) and a target axis, the input
